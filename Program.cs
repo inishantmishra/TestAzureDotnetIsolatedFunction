35f214c22e5dbd837f3de57ad2f396292199ec80
iamnishantmishra <nmishra@shipbob.com>
2023-08-20 16:13:40 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,15 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 var host = new HostBuilder()
        .ConfigureFunctionsWorkerDefaults(builder => { }, option =>
        {
            option.EnableUserCodeException = true;
        })
-        .ConfigureLogging(logging =>
-        {
-            logging.AddApplicationInsights();
-        })
        .ConfigureAppConfiguration(config => config
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("local.settings.json", optional: true)
@@ -19,15 +17,35 @@ var host = new HostBuilder()
                     .AddEnvironmentVariables())
     .ConfigureServices(services =>
     {
-        services.AddLogging(builder =>
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.Configure<LoggerFilterOptions>(options =>
         {
-            builder.AddApplicationInsights();
+            // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+            // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+            LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (toRemove is not null)
+            {
+                options.Rules.Remove(toRemove);
+            }
         });
         var serviceProvider = services.BuildServiceProvider();
 
         var configuration = serviceProvider.GetRequiredService<IConfiguration>();
 
         services.AddHttpClient();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
     })
     .Build();
 
